c26ecda663328d5cedf54e9b8f87ead9d8d4ae9b
avishek0769 <avishekadhikary42@gmail.com>
2026-03-29 21:53:01 +0530

    Set the generative and embedding models as constants

---
+++ backend/utils/constants.js
@@ -0,0 +1,16 @@
+export const EMBEDDING_MODELS = ["text-embedding-3-small", "openai/text-embedding-3-small"]
+export const LLM_MODELS = {
+    OPENAI: ["gpt-5.4", "gpt-5.4-mini", "gpt-5.4-nano", "gpt-4o"],
+    ANTHROPIC: ["claude-opus-4.6", "claude-sonnet-4.6", "claude-haiku-4.5"],
+    GOOGLE: ["gemini-3.1-pro-preview", "gemini-3-flash-preview", "gemini-3.1-flash-lite-preview"],
+    XAI: ["grok-4-0709", "grok-4.2", "grok-4-fast-reasoning"],
+    OPENROUTER: [
+        "openai/gpt-5.4",
+        "openai/gpt-5.4-mini",
+        "openai/gpt-5.4-nano",
+        "anthropic/claude-4.6-opus",
+        "anthropic/claude-4.6-sonnet",
+        "google/gemini-pro-3.1",
+        "x-ai/grok-4"
+    ]
+};
