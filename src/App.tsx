daa452e742f6835bd8b7ef757eac25fb030e14aa
Devesh <deveshpandagre9502@gmail.com>
2026-05-20 18:57:29 +0530

    fix(router): redirect authenticated users on root

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,14 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route
+                    path="/"
+                    element={
+                        <PublicOnlyRoute>
+                            <LandingPage />
+                        </PublicOnlyRoute>
+                    }
+                />
                 <Route
                     path="/dashboard"
                     element={
