f8d099584d722aa5f3a6421cec25c403f2dd0d50
dhanvin-gowda <dhanugowda4014@gmail.com>
2026-05-27 20:00:49 +0530

    added orderby

--- backend/controllers/chatMessage.controller.js
+++ backend/controllers/chatMessage.controller.js
@@ -8,12 +8,15 @@ import { qdrant, treeindex } from "../utils/ragClients.js";
 import { decryptApiKey } from "../utils/decrypt.js";
 import { generateVectorEmbeddings } from "../utils/ragUtilities.js";
 import { MemoryClient } from "mem0ai";
+import { create } from "domain";
 
 const memory = new MemoryClient({ apiKey: process.env.MEM0_API_KEY });
 
 const getAvailableModels = asyncHandler(async (req, res) => {
     const apikeys = await prisma.apiKey.findMany({
         where: { userId: req.user.id },
+        orderBy: {createdAt:"asc"},
+           
     });
     if (!apikeys.length) {
         return res
@@ -44,6 +47,7 @@ const sendMessage = asyncHandler(async (req, res) => {
     const chat = await prisma.chat.findUnique({
         where: { id: chatId },
         include: { chatSources: true },
+        orderBy: {createdAt:"asc"},   
     });
     if (!chat) {
         throw new ApiError(404, "Chat not found.");
@@ -68,6 +72,7 @@ const sendMessage = asyncHandler(async (req, res) => {
                 userId: req.user.id,
                 provider,
             },
+            orderBy: {createdAt:"asc"},  
         });
         apiKeyId = apiKey.id;
 
@@ -100,6 +105,7 @@ const sendMessage = asyncHandler(async (req, res) => {
     } else {
         const docTree = await prisma.documentTree.findUnique({
             where: { id: chat.collectionName },
+            orderBy: {createdAt:"asc"},  
         });
         treeindex.loadData(docTree.sourceData);
         treeindex.loadTree(docTree.treeData);
@@ -277,6 +283,7 @@ const exportChatMessages = asyncHandler(async (req, res) => {
 
     const chat = await prisma.chat.findUnique({
         where: { id: chatId },
+        orderBy: {createdAt:"asc"},  
     });
 
     if (!chat || chat.userId !== req.user.id) {
@@ -341,6 +348,7 @@ const getChatMessages = asyncHandler(async (req, res) => {
 
     const chat = await prisma.chat.findUnique({
         where: { id: chatId },
+        orderBy: {createdAt:"asc"},  
     });
 
     if (!chat || chat.userId !== req.user.id) {
@@ -349,6 +357,7 @@ const getChatMessages = asyncHandler(async (req, res) => {
 
     const messages = await prisma.chatMessage.findMany({
         where: { chatId },
+        orderBy: {createdAt:"asc"},  
     });
 
     if (!messages.length) {
@@ -367,6 +376,7 @@ const getChatMessageSources = asyncHandler(async (req, res) => {
 
     const messageSources = await prisma.chatMessageSource.findMany({
         where: { chatMessageId: messageId },
+        orderBy: {createdAt:"asc"},  
     });
 
     if (!messageSources.length) {
